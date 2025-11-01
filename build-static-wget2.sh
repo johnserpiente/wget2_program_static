a02a82234743553b42cb421761b47a5eb788b6d0
John Serpiente <42440075+johnserpiente@users.noreply.github.com>
2025-11-01 01:08:34 +0100

    Update wget2 build script for dependency versions

--- build-static-wget2.sh
+++ build-static-wget2.sh
@@ -10,23 +10,20 @@ mkdir -p /work/artifact
 
 # =========================================================================
 # 1. DEPENDENCIAS CRÍTICAS PARA GNUTLS (GMP, Nettle)
-# Deben compilarse primero, ya que GnuTLS depende de ellas.
 # =========================================================================
 
 echo "--- Compilando GMP ---"
 cd $WORKSPACE
 curl -sL https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz | tar x --xz
 cd gmp-6.3.0
-# --disable-assembly mejora la portabilidad
 LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr --disable-shared --enable-static --disable-assembly
 make -j$(nproc)
 make install
 
 echo "--- Compilando Nettle ---"
 cd $WORKSPACE
-curl -sL https://ftp.gnu.org/gnu/nettle/nettle-3.10.2.tar.gz | tar x --gzip
-cd nettle-3.10.2
-# Necesita GMP, que acabamos de instalar
+curl -sL https://ftp.gnu.org/gnu/nettle/nettle-3.9.1.tar.gz | tar x --gzip
+cd nettle-3.9.1
 LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr --disable-shared --enable-static --disable-documentation --disable-openssl --enable-gmp
 make -j$(nproc)
 make install
@@ -36,24 +33,23 @@ make install
 # =========================================================================
 
 echo "--- Compilando libidn2 ---"
-# Se compila aquí porque GnuTLS la necesita
 cd $WORKSPACE
 curl -sL https://ftp.gnu.org/gnu/libidn/libidn2-2.3.8.tar.gz | tar x --gzip
 cd libidn2-2.3.8
-# Usamos su libunistring incluida para simplificar
 LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr --disable-doc --disable-nls --disable-shared --with-included-libunistring
 make -j$(nproc)
 make install
 
 echo "--- Compilando GnuTLS ---"
-# Ahora que GMP, Nettle y libidn2 están listas, compilamos GnuTLS
+# Se usa la versión estable 3.8.5. Tu log mostraba 3.8.10, que puede ser de desarrollo.
 cd $WORKSPACE
-curl -sL https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.10.tar.xz | tar x --xz
-cd gnutls-3.8.10
-# Añadimos CFLAGS="-Wno-analyzer-infinite-loop" para silenciar la advertencia
-# que detiene la compilación con compiladores modernos.
-# También añadimos --disable-werror por si acaso.
-CFLAGS="-Wno-analyzer-infinite-loop" LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr \
+curl -sL https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.5.tar.xz | tar x --xz
+cd gnutls-3.8.5
+
+# ==================== SOLUCIÓN DEFINITIVA AQUÍ ====================
+# Usamos CFLAGS="-fno-analyzer" para desactivar por completo el analizador estático
+# de GCC, que es el que genera la avalancha de advertencias y ralentiza todo.
+CFLAGS="-fno-analyzer" LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr \
     --with-included-libtasn1 \
     --without-p11-kit \
     --without-tpm \
@@ -64,6 +60,7 @@ CFLAGS="-Wno-analyzer-infinite-loop" LDFLAGS="-static --static -no-pie -s" ./con
     --with-crypto-backend=nettle \
     --disable-tests \
     --disable-werror
+# ==================================================================
 make -j$(nproc)
 make install
 
@@ -112,10 +109,6 @@ git clone https://github.com/rockdaboot/wget2
 cd wget2
 git clone --recursive https://github.com/coreutils/gnulib.git
 ./bootstrap
-
-# CAMBIOS PRINCIPALES:
-# 1. Se cambia '--with-ssl=openssl' por '--with-ssl=gnutls'.
-# 2. Se añaden las librerías de GnuTLS, Nettle y GMP a LDFLAGS. El orden es importante.
 LDFLAGS="-static --static -no-pie -s -lgnutls -lnettle -lhogweed -lgmp -lidn2 -lunistring -lbrotlienc -lbrotlidec -lbrotlicommon -lgpgmepp -lgpgme -lgpg-error -lassuan" \
 ./configure --with-ssl=gnutls --with-lzma --with-gpgme --prefix=/usr/local/wget2mm --with-bzip2 --enable-manylibs --disable-shared
 make -j$(nproc)
@@ -129,14 +122,9 @@ echo "--- Empaquetando el artefacto ---"
 cd /usr/local
 echo "Tamaño del binario antes de UPX:"
 ls -l -R wget2mm/bin
-
-# Comprimir el binario con UPX para un tamaño mínimo
 upx --keep --best --lzma wget2mm/bin/wget2
-
 echo "Tamaño del binario después de UPX:"
 ls -l -R wget2mm/bin
-
-# Crear el archivo tar.gz final solo con el ejecutable
 tar czf ./wget2.tar.gz wget2mm/bin/wget2
 mv ./wget2.tar.gz /work/artifact/
 
